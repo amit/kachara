c4043fe1572b57dd38e231c2309c5f9ebf64332b
Amit Chakradeo <amit@chakradeo.net>
2009-04-21 15:41:10 -0700

    The opml files can not be remote!

--- opml2csv.rb
+++ opml2csv.rb
@@ -1,17 +1,21 @@
 require 'csv'
+require 'open-uri'
 require "rexml/document"
 include REXML
 if ARGV.length >=1
 fname = ARGV[0] 
 else
-fname = "opml.xml"
+#fname = "opml.xml"
+puts "Usage: #{$0} opml_file_or_url"
+exit
 end
 
-doc = Document.new File.new(fname)
+handle=open(fname)
+doc = Document.new handle
 CSV.open('csvfile.csv', 'w') do |writer|
 writer << ["URL", "name"]
 doc.elements.each("//outline[@type='rss']") {|element|
-writer << [element.attribute("xmlUrl").value, element.attribute("title").value]
+writer << [element.attribute("xmlUrl").value, element.attribute("text").value]
 }
 end
 
