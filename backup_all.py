f38b127e80ad4359e6dcdf142f9bf961171494df
Amit Chakradeo <amit@chakradeo.net>
2009-11-16 16:33:36 -0800

    Added util to backup everydns records for all domains. Needs everydnslib (http://hostingfu.com/article/everydns-python-api-and-command-shell)

---
+++ backup_all.py
@@ -0,0 +1,12 @@
+import everydnslib,yaml
+from time import strftime
+filename="dnsbackup-"+strftime("%Y%m%d-%H%M%S")+".yml"
+data=yaml.load(file('config.yml').read())
+e=everydnslib.EveryDNS(data['username'], data['password'])
+records = e.cache_domains()
+for record in records:
+    record['records'] = e.cache_records(record['domain'])
+f= open(filename, "w")
+yaml.dump(records,f)
+f.close()
+
